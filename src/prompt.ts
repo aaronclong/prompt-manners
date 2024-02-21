5d10e77a44ef198ebb8646f8c8b335d9a72d796a
Aaron Long <aaron.long.c@gmail.com>
2024-02-20 21:17:07 -0500

    feat: Iterate on template idea

--- src/prompt.ts
+++ src/prompt.ts
@@ -1,3 +1,31 @@
+export const PromptSymbol = Symbol.for("prompt-tag");
+
+// TODO: Terms to flush out
+// data prompt
+// instruction prompt
+
+export function instructionPrompt(
+  literals: TemplateStringsArray,
+  ...keys: unknown[]
+) {
+  const rawPrompt = literals
+    .map((literal, idx) => {
+      let result = literal;
+      if (idx < keys.length) {
+        result += keys[idx] as string;
+      }
+
+      return result;
+    })
+    .join("")
+    .trim();
+
+  return {
+    instruction: rawPrompt,
+    [PromptSymbol]: "instruction",
+  };
+}
+
 export function prompt(literals: TemplateStringsArray, ...keys: unknown[]) {
   const rawPrompt = literals
     .map((literal, idx) => {
