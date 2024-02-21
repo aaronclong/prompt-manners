7622f8cf61b4a03738fa90133aa951296a81734f
Aaron Long <aaron.long.c@gmail.com>
2024-02-20 21:17:26 -0500

    test: Iterate on unit tests

--- src/prompt.test.ts
+++ src/prompt.test.ts
@@ -1,10 +1,28 @@
 import test from "ava";
 
-import { prompt } from "./prompt.js";
-
-const fn = () => "foo";
+import { prompt, instructionPrompt, PromptSymbol } from "./prompt.js";
 
 test("prompt, happy path", (t) => {
   const { rawPrompt } = prompt`foo`;
   t.is(rawPrompt, "foo");
+});
+
+test.skip("prompt, parse slot correctly", (t) => {
+  const { rawPrompt } = prompt`
+  Answer the following question:<slot>
+  Slot: Find the answer to life, the universe, and everything.
+`;
+  t.is(rawPrompt, "foo");
+});
+
+test("instructionPrompt, parse slot correctly", (t) => {
+  const instruction = instructionPrompt`
+   Give a recommendation based on the user's request
+  `;
+  t.not(instruction, null);
+  t.is(instruction[PromptSymbol], "instruction");
+  t.is(
+    instruction.instruction,
+    "Give a recommendation based on the user's request"
+  );
 });
