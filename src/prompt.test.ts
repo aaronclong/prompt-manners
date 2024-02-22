a188f23dbecebb54599f21a0a7d8b6a39ed39c6c
Aaron Long <aaron.long.c@gmail.com>
2024-02-21 23:30:37 -0500

    test: Get unit tests passing

--- src/prompt.test.ts
+++ src/prompt.test.ts
@@ -3,16 +3,18 @@ import test from "ava";
 import { prompt, instructionPrompt, PromptSymbol } from "./prompt.js";
 
 test("prompt, happy path", (t) => {
-  const { rawPrompt } = prompt`foo`;
-  t.is(rawPrompt, "foo");
+  const { renderPrompt } = prompt`foo`;
+  const p = renderPrompt();
+  t.falsy(p.value);
+  t.false(p.isPresent);
 });
 
-test.skip("prompt, parse slot correctly", (t) => {
-  const { rawPrompt } = prompt`
-  Answer the following question:<slot>
+test("prompt, parse slot correctly", (t) => {
+  const { renderPrompt } = prompt`
+  ${instructionPrompt`Answer the following question`}:<slot>
   Slot: Find the answer to life, the universe, and everything.
 `;
-  t.is(rawPrompt, "foo");
+  t.is(renderPrompt().value, "Answer the following question");
 });
 
 test("instructionPrompt, parse slot correctly", (t) => {
--- src/prompt.ts
+++ src/prompt.ts
@@ -1,18 +1,31 @@
+import { Optional } from "./lib-core.js";
+
 export const PromptSymbol = Symbol.for("prompt-tag");
 
 // TODO: Terms to flush out
 // data prompt
 // instruction prompt
 
+type PromptTypes = "instruction" | "data";
+
+export interface Prompt {
+  [PromptSymbol]: PromptTypes;
+}
+
+export interface InstructionPrompt extends Prompt {
+  instruction: string;
+  [PromptSymbol]: "instruction";
+}
+
 export function instructionPrompt(
   literals: TemplateStringsArray,
-  ...keys: unknown[]
-) {
+  ...args: unknown[]
+): InstructionPrompt {
   const rawPrompt = literals
     .map((literal, idx) => {
       let result = literal;
-      if (idx < keys.length) {
-        result += keys[idx] as string;
+      if (idx < args.length) {
+        result += args[idx] as string;
       }
 
       return result;
@@ -26,19 +39,37 @@ export function instructionPrompt(
   };
 }
 
-export function prompt(literals: TemplateStringsArray, ...keys: unknown[]) {
-  const rawPrompt = literals
-    .map((literal, idx) => {
-      let result = literal;
-      if (idx < keys.length) {
-        result += keys[idx] as string;
-      }
+export function prompt(_literals: TemplateStringsArray, ...args: unknown[]) {
+  // The question is how do zip them
+  // Since this could start as an arg and continue?
+  // for this tag the args might be more important
+  // This ordering might not matter because we are just using this to compile all the prompts to one
+  const prompts = new Map<PromptTypes, Prompt>();
 
-      return result;
-    })
-    .join("");
+  for (const arg of args) {
+    if (Object.hasOwn(arg as object, PromptSymbol)) {
+      prompts.set((arg as object)[PromptSymbol], arg as Prompt);
+    }
+  }
+
+  /**
+   * This will always render in this order
+   * 1. instruction prompt
+   * 2. data prompt
+   */
+  const renderPrompt = (): Optional<string> => {
+    if (!prompts.has("instruction")) {
+      return Optional.empty();
+    }
+
+    let result = "";
+    const instruction = prompts.get("instruction") as InstructionPrompt;
+    result += instruction.instruction;
+
+    return Optional.of(result);
+  };
 
   return {
-    rawPrompt,
+    renderPrompt,
   };
 }
