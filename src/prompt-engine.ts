57f0dfd2ecbf7ac4cbd794c0c18cdadb656f6c4d
Aaron Long <aaron.long.c@gmail.com>
2024-03-04 23:11:22 -0500

    refactor: Setup  paraphrasing & other preprocess

--- src/prompt-engine.ts
+++ src/prompt-engine.ts
@@ -2,6 +2,7 @@ import {
   loadDefaultTokenizer,
   ITokenizer,
 } from "./defense-strategies/index.js";
+import { Optional } from "./lib-core.js";
 import { PromptCompiler } from "./prompt.js";
 import { isFalsy } from "./utils.js";
 
@@ -9,6 +10,74 @@ export interface PromptEngineConfig {
   modelName: string;
   retokenize?: ITokenizer | boolean;
   maxInputLength?: number;
+}
+
+export type PromptStep = "paraphrase" | "ready";
+
+export interface IPrompt {
+  getSteps(): PromptStep[];
+  addResult(step: PromptStep, value: string);
+  render(): Promise<Optional<string>>;
+}
+
+type PromptStepStatus = "incomplete" | "complete";
+
+export class Prompt implements IPrompt {
+  private stepStates: Map<
+    PromptStep,
+    { status: PromptStepStatus; value: string }
+  >;
+  constructor(
+    private readonly dataInput: string,
+    private readonly steps: PromptStep[],
+    private readonly promptCompiler: PromptCompiler,
+    private readonly tokenizer?: ITokenizer
+  ) {
+    this.stepStates = new Map(
+      steps.map((step) => [step, { status: "incomplete", value: "" }])
+    );
+  }
+
+  getSteps = (): PromptStep[] => {
+    return [...this.steps];
+  };
+
+  addResult = (step: PromptStep, value: string) => {
+    this.stepStates.set(step, { status: "complete", value });
+  };
+
+  render = async (): Promise<Optional<string>> => {
+    const paraphrase = this.paraphrasePrompt();
+
+    const data = paraphrase.isPresent
+      ? (paraphrase.value as string)
+      : this.dataInput;
+
+    const fmtInput = (await this.runTokenization(data)) ?? data;
+    return this.promptCompiler.renderPrompt(fmtInput);
+  };
+
+  private runTokenization = async (input: string) => {
+    const encoding = await this.tokenizer?.encode(input);
+    return encoding?.getTokens()?.join(" ");
+  };
+
+  private paraphrasePrompt = (): Optional<string> => {
+    // this.input;
+    // return this.promptCompiler.renderPrompt(fmtInput);
+    const paraphrase = this.stepStates.has("paraphrase")
+      ? this.stepStates.get("paraphrase")
+      : undefined;
+
+    if (paraphrase && paraphrase?.status !== "complete") {
+      return this.promptCompiler.preProcessData(this.dataInput);
+    }
+
+    if (paraphrase && paraphrase?.status === "complete") {
+      return Optional.of(paraphrase.value);
+    }
+    return Optional.empty();
+  };
 }
 
 export class PromptEngine {
